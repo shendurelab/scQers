ff9527cde76404b6c130b01045bef976acce0c07
Maggie Maurer <87448603+maurermaggie@users.noreply.github.com>
2024-04-29 13:07:54 -0700

    Update run_snakemake.sh

--- scripts/MPRA_Snakemake_Pipeline/workflow/run_snakemake.sh
+++ scripts/MPRA_Snakemake_Pipeline/workflow/run_snakemake.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+#################################---Step 1: Load Conda Snakemake Environemnt---###################################################
 if conda info --envs | grep -q maggie_snakemake; then echo "maggie_snakemake environment already exists"; else conda env create -f environment.yaml; fi
 
 . "$HOME/micromamba/etc/profile.d/mamba.sh"
@@ -7,6 +8,7 @@ if conda info --envs | grep -q maggie_snakemake; then echo "maggie_snakemake env
 
 mamba activate maggie_snakemake
 
+#########################################---Step 2: Define Functions---###########################################################
 check_for_config_file() {
     argument_name="${1}"
     file_path="${2}"
@@ -16,9 +18,16 @@ check_for_config_file() {
     fi
 }
 
-##################################################################################################################################
-#############################################---Step 1: set up parameters---######################################################
-##################################################################################################################################
+check_for_profile() {
+    argument_name="${1}"
+    directory_path="${2}"
+    if [[ ! -d ${directory_path} ]]; then
+        echo "Error: filepath ${directory_path} passed with ${argument_name} does not exist."
+        exit 1
+    fi
+}
+
+#############################################---Step 3: Set Up Parameters---######################################################
 options_array=(
     config_file
     profile
@@ -34,7 +43,7 @@ while true; do
         --config_file )
             config_file="${2}"; check_for_config_file "${1}" "${2}"; shift 2 ;;
         --profile )
-            profile="${2}"; shift 2 ;;
+            profile="${2}"; check_for_profile "${1}" "${2}"; shift 2 ;;
         -- )
             shift; break ;;
         * )
@@ -42,17 +51,24 @@ while true; do
             exit 1
     esac
 done
+    
+########################################---Step 4: Get Number of Cores---#########################################################
+#This is based on the number of directories in the input_directory (as defined by the config file)
+no_cores_filepath=$(awk -F'"' '/input_directory/{print $2}' ${config_file})
+no_cores=$(find ${no_cores_filepath}/* -maxdepth 0 -type d | wc -l)
 
-echo "input files read"
+##########################################---Step 4: Clean yaml file---############################################################
+#The script will produce an error if there are double slashes in filepaths. Thus, for all directories, they must NOT end in a /
+#This code replaces all instances of /" with " in the yaml file.
+sed -i 's/\/"/"/g' ${config_file}
 
-if [ -z $profile ]; then
+###########################################---Step 4: Run Snakemake---############################################################
+if [ -z $config_file ]; then
+    echo "no config_file selected; please select config_file to run snakemake on"
+    exit 1
+elif [ -z $profile ]; then
     echo "no profile selected"
-    echo "job will be run sequentially"
-    snakemake --configfile ${config_file} -c 1
-else
-    if [ ! -d $profile ]; then
-        echo "profile directory (${profile}) cannot be found"
-    else
-        snakemake --configfile ${config_file} -c 1 --profile ${profile}
-    fi
+    snakemake --configfile ${config_file} -c ${no_cores} --use-conda
+else 
+    snakemake --configfile ${config_file} -c ${no_cores} --profile ${profile} --use-conda
 fi
